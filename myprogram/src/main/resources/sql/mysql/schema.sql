f86b4fb1b2711f84e1043a0e50317fa93b20c4b0
microlcl <lichleicy@gmail.com>
2013-11-01 23:29:01 +0800

    Adding some table schemas for h2 and mysql

--- myprogram/src/main/resources/sql/mysql/schema.sql
+++ myprogram/src/main/resources/sql/mysql/schema.sql
@@ -1,21 +1,87 @@
-drop table if exists ss_task;
-drop table if exists ss_user;
+drop table if exists users;
+drop table if exists category;
+drop table if exists modules;
+drop table if exists functions;
+drop table if exists role_function;
+drop table if exists departments;
+drop table if exists user_department;
 
-create table ss_task (
-	id bigint auto_increment,
-	title varchar(128) not null,
-	description varchar(255),
-	user_id bigint not null,
-    primary key (id)
-) engine=InnoDB;
 
-create table ss_user (
-	id bigint auto_increment,
-	login_name varchar(64) not null unique,
-	name varchar(64) not null,
-	password varchar(255) not null,
-	salt varchar(64) not null,
-	roles varchar(255) not null,
-	register_date timestamp not null default 0,
-	primary key (id)
-) engine=InnoDB;
+CREATE TABLE `category` (
+  `id` varchar(64) NOT NULL,
+  `pid` varchar(64) DEFAULT NULL,
+  `name` varchar(64) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `trashed` varchar(1) NOT NULL,
+  `comment` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `departments` (
+  `department_id` varchar(64) NOT NULL,
+  `pid` varchar(64) NOT NULL,
+  `description` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`department_id`),
+  UNIQUE KEY `department_id` (`department_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `functions` (
+  `function_id` varchar(64) NOT NULL,
+  `module_id` varchar(64) DEFAULT NULL,
+  `name` varchar(64) NOT NULL,
+  `description` varchar(128) DEFAULT NULL,
+  `path` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`function_id`),
+  UNIQUE KEY `function_id` (`function_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `modules` (
+  `module_id` varchar(64) NOT NULL DEFAULT '',
+  `pid` varchar(64) DEFAULT NULL,
+  `name` varchar(64) NOT NULL,
+  `description` varchar(128) DEFAULT NULL,
+  `path` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`module_id`),
+  UNIQUE KEY `moduleId` (`module_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `role_function` (
+  `role_id` varchar(64) NOT NULL,
+  `function_id` varchar(64) NOT NULL,
+  `description` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`role_id`,`function_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `roles` (
+  `role_id` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `description` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `role_id` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_department` (
+  `user_id` varchar(64) NOT NULL,
+  `department_id` varchar(64) NOT NULL,
+  PRIMARY KEY (`user_id`,`department_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `sex` varchar(2) DEFAULT NULL,
+  `departent_id` varchar(64) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `phone_num` varchar(20) DEFAULT NULL,
+  `address` varchar(64) DEFAULT NULL,
+  `howmtown` varchar(64) DEFAULT NULL,
+  `birthday` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `register_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `status` varchar(20) DEFAULT NULL,
+  `comment` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
