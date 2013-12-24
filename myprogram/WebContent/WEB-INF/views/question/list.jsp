e96d3b5d08d2d5d7e2921690dab71f0bc03011bf
DevinYDQ <460953643@qq.com>
2013-12-24 17:37:10 +0800

    问题管理页面

---
+++ myprogram/WebContent/WEB-INF/views/question/list.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="tags" tagdir="/WEB-INF/tags" %>
+<%@ taglib uri="http://com.eastteam.myprogram/mytaglib" prefix="mytag" %>
+<c:set var="ctx" value="${pageContext.request.contextPath}"/>
+<html>
+<head>
+<title>问题管理</title>
+</head>
+<body>
+
+</body>
+</html>
--- myprogram/src/main/java/com/eastteam/myprogram/web/questions/QuestionController.java
+++ myprogram/src/main/java/com/eastteam/myprogram/web/questions/QuestionController.java
@@ -1,5 +1,18 @@
 package com.eastteam.myprogram.web.questions;
 
-public class QuestionController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping (value = "/question")
+public class QuestionController {
+	private static Logger logger = LoggerFactory.getLogger(QuestionController.class);
+	
+	@RequestMapping (value = "list", method = RequestMethod.GET)
+	public String list(){
+		return "question/list";
+	}
 }
